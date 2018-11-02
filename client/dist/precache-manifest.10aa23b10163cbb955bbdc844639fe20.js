b1a8ea3f7d33eee4199e2b2ac366b2bf2d5b4daa
subham ojha <subham.ojha@multidots.com>
2018-11-02 16:46:16 +0530

    prod build

---
+++ client/dist/precache-manifest.10aa23b10163cbb955bbdc844639fe20.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "58c59a917d49063272af",
+    "url": "main.js"
+  },
+  {
+    "revision": "58c59a917d49063272af",
+    "url": "main.css"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "07295baafff72cc15856d42c47c4b4e7",
+    "url": "./index.html"
+  }
+];
